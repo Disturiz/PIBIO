f823b24b993202b107c64acdbf3770421c960ddc
Disturiz <52639169+Disturiz@users.noreply.github.com>
2022-11-09 20:37:09 -0600

    Add files via upload

--- calcularProductoInternoBruto.js
+++ calcularProductoInternoBruto.js
@@ -12,9 +12,11 @@ const tCAPITAL = document.querySelector("#totalCapital");
 const pMining = document.querySelector("#porcenmining");
 const pFactory = document.querySelector("#porcenfactory");
 const pEstate = document.querySelector("#porcenestate");
-const pCommerce = document.querySelector("#porcenestate");
+const pCommerce = document.querySelector("#porcencommerce");
 const pCommunic = document.querySelector("#porcencommunication");
 const pBanking = document.querySelector("#porcenbanking");
+const pais = document.querySelector("#country");
+
 
 
 
@@ -30,6 +32,7 @@ function calculoPib() {
     let communicationValue = Number(communication.value);
     let bankingValue = Number(banking.value);
     let populationValue = Number(population.value);
+    
 
     console.log ({
         miningValue,
@@ -79,11 +82,11 @@ function calculoPib() {
 
     // Calculo de PIB
     const total = miningValue + factoryValue + estateValue + commerceValue + communicationValue + bankingValue;
-    const tPIB = total;
-    console.log("total PIB: ", {tPIB});
+    const totalPIB = total;
+    console.log("total PIB: ", {totalPIB});
 
     // Cáculo ingreso Per Capital
-    const tCAPITAl =  (miningValue + factoryValue + estateValue + commerceValue + communicationValue + bankingValue) / populationValue;
+    const tCAPITAl =  totalPIB / populationValue;
     console.log("total Percapital: ", {tCAPITAl})
     let totalElementos = [];
     totalElementos.push(mining.value);
@@ -92,37 +95,75 @@ function calculoPib() {
     totalElementos.push(commerce.value);
     totalElementos.push(communication.value);
     totalElementos.push(banking.value);
-    console.log({totalElementos});
+
+    console.log("tCAPITAl", {tCAPITAl});
 
     // Calcula los porentajes de los elementos del PIB
-    const pMining = ((mining.value * 100) / tPIB).toFixed();
-    const pFactory = ((factory.value * 100) / tPIB).toFixed();
-    const pEstate = ((estate.value * 100) / tPIB).toFixed();
-    const pCommerce = commerce.value * 100 / tPIB;
-    const pBanking = ((banking.value * 100) / tPIB).toFixed();
-    const pCommunic = ((communication.value * 100) / tPIB).toFixed();
-    console.log("total Mineria: ", {pMining});
-    console.log("total Manufactura: ", {pFactory});
-    console.log("total Comercios: ", {pCommerce});
-    console.log("total Cumunicaciones: ", {pCommunic}); 
-    console.log("total Finanzas: ", {pBanking});
-    
+    const tMining = ((mining.value * 100) / totalPIB).toFixed();
+    const tFactory = ((factory.value * 100) / totalPIB).toFixed();
+    const tEstate = ((estate.value * 100) / totalPIB).toFixed();
+    const tCommerce = ((commerce.value * 100) / totalPIB).toFixed();
+    const tBanking = ((banking.value * 100) / totalPIB).toFixed();
+    const tCommunic = ((communication.value * 100) / totalPIB).toFixed();
 
-    if(tPIB != 0){
-        tPIB.innerText = '$' + tPIB;
+    if(totalPIB != 0){
+        tPIB.style.color = 'white';
+        tPIB.innerText = 'Mill$ ' + totalPIB;
     }
     else {
+        tPIB.style.color = 'red';
         tPIB.innerText = 'No existen elementos para calcular PIB';
     }
 
-    if(tCAPITAL != 0){
-        tCAPITAL.innerText = '$' + tCAPITAl;
+    if(tCAPITAl != 0){
+        tCAPITAL.style.color = 'white'
+        tCAPITAL.innerText = '$ ' + tCAPITAl.toFixed(2);
     }
     else {
-        tCAPITAL.innerText = 'No existen elementos para calcular PIB';
+        tCAPITAL.style.color = 'red';
+        tCAPITAL.innerText = 'No existe Igreso Per Capital';
     }
 
-    pMining.innerText =  '%' + pMining;
-    pBanking.innerText = '%' + pBanking;
+    pMining.innerText = tMining + '%';
+    pFactory.innerText = tFactory + '%';
+    pEstate.innerText = tEstate + '%';
+    pCommerce.innerText = tCommerce + '%';
+    pCommunic.innerText = tCommunic + '%';
+    pBanking.innerText = tBanking + '%';
+
+    const ctx = document.getElementById('myChart');
+    const elementos = ['Mineria','Manufactura','Inmobilarios', 'Comercios', 'Comunicaciones', 'Financieras'];
+    const porcentajes = totalElementos.map(function(element){
+        return (element * 100)/totalPIB
+    });
+    console.log("Porcentajes: ", {porcentajes}); 
     
+    
+    const myChart = new Chart(ctx,{
+        type: 'pie',
+        data: {
+            labels: elementos,
+            datasets: [{
+                label: 'Distribucion',
+                data: porcentajes,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235 ,0.2)',
+                    'rgba(75, 192, 192 ,0.2)',
+                    'rgba(153, 102,255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(100, 102, 155, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',   
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(100, 102, 155, 1)'
+                ],
+                borderWidth: 1.5
+            }]
+        }
+    })
 }
